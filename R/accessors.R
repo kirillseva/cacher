fb3185dab01c4564e097a38f2308493dfbbdb31a
Abel Castillo <abelcastillomath@gmail.com>
2018-03-03 19:42:00 -0600

    looks like this is easier than I thought

--- R/accessors.R
+++ R/accessors.R
@@ -1,13 +1,2 @@
-`[[` <- function(cache, key) { UseMethod("[[") }
-
-`[[.default` <- .Primitive("[[")
-
 `[[.LRUcache_` <- function(cache, key) { cache$get(key) }
-
-
-
-`[[<-` <- function(cache, key, obj) { UseMethod("[[<-") }
-
-`[[<-.default` <- .Primitive("[[<-")
-
 `[[<-.LRUcache_` <- function(cache, key, obj) { cache$set(key, obj) }
